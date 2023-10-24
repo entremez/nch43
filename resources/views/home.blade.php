23f17f163b8abb63b91d328aa66bc37fc7bf0d0a
sicalingenieros <m.espinoza@sicalingenieros.com>
2023-10-24 13:45:42 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -413,7 +413,7 @@
                         <p class="mt-6 text-base">El funcionamiento es el siguiente: Se deben cambiar los valores entre corchetes de la URL acorde a lo que se desea revisar.</p>
                         <p class="mt-3 text-base">La URL tiene la siguiente forma:</p>
                         <p class="mt-3 text-xl">https://nch43.cl/[lote]/[muestras]/[fila_inidical]/[columna_inicial]</p>
-                        <p class="mt-6 text-base">Por lo tanto para recrear los ejemplos N°3 de la norma NCh44 las URL serán:
+                        <p class="mt-6 text-base">Por lo tanto para recrear los ejemplos N°3 de la norma NCh43 las URL serán:
                         <p class="mt-6 text-base underline">a. <a href="https://nch43.cl/15/11/250/17">https://nch43.cl/15/11/250/17</a></p>
                         <p class="mt-3 text-base underline">b. <a href="https://nch43.cl/80/4/1/11">https://nch43.cl/80/4/1/11</a></p>
                         <p class="mt-3 text-base underline" >c. <a href="https://nch43.cl/180/10/2/15">https://nch43.cl/180/10/2/15</a></p>
