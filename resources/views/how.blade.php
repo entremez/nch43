b9dd0e0c89bd6d49d278a7aca2c09dc5be6b456e
sicalingenieros <m.espinoza@sicalingenieros.com>
2023-11-09 09:57:09 -0300

    update title

--- resources/views/how.blade.php
+++ resources/views/how.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>NCh43 - mez</title>
+    <title>NCh43.cl</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -410,10 +410,10 @@
     <div class="px-8 mt-9">
                     <div class="">
                         <h1 class="text-4xl font-bold ">NCh43 Online</h1>
-                        <p class="mt-6 text-base">El funcionamiento es el siguiente: Se deben cambiar los valores entre corchetes de la URL acorde a lo que se desea revisar.</p>
+                        <p class="mt-6 text-base">El funcionamiento es el siguiente: Se deben cambiar los valores entre corchetes [] de la URL de acuerdo con el muestreo a realizar.</p>
                         <p class="mt-3 text-base">La URL tiene la siguiente forma:</p>
-                        <p class="mt-3 text-xl">https://nch43.cl/[lote]/[muestras]/[fila_inidical]/[columna_inicial]</p>
-                        <p class="mt-6 text-base">Por lo tanto para recrear los ejemplos N°3 de la norma NCh43 las URL serán:
+                        <p class="mt-3 text-xl">https://nch43.cl/[lote]/[muestras]/[fila_inicial]/[columna_inicial]</p>
+                        <p class="mt-6 text-base">Por lo tanto para recrear los ejemplos N°3 de la norma NCh43 las URL serían:
                         <p class="mt-6 text-base underline">a. <a href="https://nch43.cl/15/11/250/17">https://nch43.cl/15/11/250/17</a></p>
                         <p class="mt-3 text-base underline">b. <a href="https://nch43.cl/80/4/1/11">https://nch43.cl/80/4/1/11</a></p>
                         <p class="mt-3 text-base underline" >c. <a href="https://nch43.cl/180/10/2/15">https://nch43.cl/180/10/2/15</a></p>
--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>NCh43 - mez</title>
+        <title>NCh43.cl</title>
 
         <!-- Fonts -->
 
