c06ec5a7aecff5575b20a1ae051a3f81fcc8c567
sicalingenieros <m.espinoza@sicalingenieros.com>
2024-09-04 13:35:07 -0400

    Update welcome2.blade.php

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>NCh43.cl</title>
+        <title>NCh43.cl - Exportar</title>
 
         <!-- Fonts -->
 
