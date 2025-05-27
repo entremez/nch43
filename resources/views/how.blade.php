28fc5cccf0c027a138e2a6a71f8a31150fa3e89b
sicalingenieros <m.espinoza@sicalingenieros.com>
2025-05-27 11:02:42 -0400

    delete some unnecessary files and fix url error

--- resources/views/how.blade.php
+++ resources/views/how.blade.php
@@ -10,9 +10,9 @@
                         <p class="mt-3 text-base">La URL tiene la siguiente forma:</p>
                         <p class="mt-3 text-xl">https://nch43.cl/[lote]/[muestras]/[fila_inicial]/[columna_inicial]</p>
                         <p class="mt-6 text-base">Por lo tanto para recrear los ejemplos N°3 de la norma NCh43 las URL serían:
-                        <p class="mt-6 text-base underline">a. <a href="https://nch43.cl/15/11/250/17">https://nch43.cl/15/11/250/17</a></p>
-                        <p class="mt-3 text-base underline">b. <a href="https://nch43.cl/80/4/1/11">https://nch43.cl/80/4/1/11</a></p>
-                        <p class="mt-3 text-base underline" >c. <a href="https://nch43.cl/180/10/2/15">https://nch43.cl/180/10/2/15</a></p>
+                        <p class="mt-6 text-base underline">a. <a href="/15/11/250/17">https://nch43.cl/15/11/250/17</a></p>
+                        <p class="mt-3 text-base underline">b. <a href="/80/4/1/11">https://nch43.cl/80/4/1/11</a></p>
+                        <p class="mt-3 text-base underline" >c. <a href="/180/10/2/15">https://nch43.cl/180/10/2/15</a></p>
                     </div>
 </main>
 
